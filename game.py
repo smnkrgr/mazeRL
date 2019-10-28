4cea6af30861b60f45b1c3a0573d612009b49b77
smnkrgr <simon.krueger@student.uni-augsburg.de>
2019-10-28 19:56:38 +0100

    Setup game script

---
+++ game.py
@@ -0,0 +1,16 @@
+from maze import Maze
+from tui import Tui
+
+if __name__ == "__main__":
+
+    maze = Maze("maze.txt")
+    tui = Tui()
+    status = "OK"
+
+    while status != "GOAL":
+        pos_y, pos_x = maze.getCurrentPosition()
+        tui.draw(maze.getMaze(), pos_y, pos_x)
+        status = maze.handleMove(tui.recieveInput())
+
+    print "finished"
+    
--- tui.py
+++ tui.py
@@ -1,17 +1,37 @@
+import os
 
 class Tui:
 
     def __init__(self):
+
         return
 
-    def draw(self, field, player_pos):
+    def draw(self, field, player_pos_y, player_pos_x):
+
+        field[player_pos_y][player_pos_x] = "O"
+        os.system("clear")
+
+        for i in field:
+            print("".join(i)) 
+
+        field[player_pos_y][player_pos_x] = " "
+
         return
 
     def recieveInput(self):
-        player_input = input()
-        print player_input
-        return
 
+        print "1 = left, 2 = down, 3 = up and 4 = right. Waiting for input:"
+        try:
+            player_input = input()
+        except:
+            player_input = 5
+        
+        if player_input == 1: return "LEFT"
+        elif player_input == 2: return "DOWN"
+        elif player_input == 3: return "UP"
+        elif player_input == 4: return "RIGHT"
+        else:
+            print "Wrong Input; try again"
+            return self.recieveInput()
+        
 
-tui = Tui()
-tui.recieveInput()
