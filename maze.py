27da3e3f56618a1465fa3a9b75c872fc42204ac8
smnkrgr <simon.krueger@student.uni-augsburg.de>
2019-10-28 12:58:10 +0100

    Add gitignore and clean up

--- maze.py
+++ maze.py
@@ -6,12 +6,15 @@ class Maze:
     goal_position_char = "X"
     
     def __init__(self, path):
+
         self.maze = self.readMaze(path)
         self.current_position_y, self.current_position_x = self.setStartingPosition()
         self.goal_position = self.setGoalPosition()
 
     def handleMove(self, move):
+
         status = "OK"
+
         if move == "UP" and self.checkLegalMove(self.current_position_y - 1, self.current_position_x):
             self.current_position_y = self.current_position_y - 1 
         elif move == "DOWN" and self.checkLegalMove(self.current_position_y + 1, self.current_position_x):
@@ -25,19 +28,24 @@ class Maze:
         return status
 
     def checkLegalMove(self, pos_y, pos_x):
+
+        if pos_y > len(self.maze[0]): return False
+        if pos_x > len(self.maze): return False
+        
         if self.maze[pos_y][pos_x] == " ":
             return True
         elif self.maze[pos_y][pos_x] == self.starting_position_char:
             return True
         elif self.maze[pos_y][pos_x] == self.goal_position_char:
             return True
-        else:
-            return False
+        else: return False
 
     def readMaze(self, path):
+
         maze = []
         mazeFile = open(path, "r")
         columns = mazeFile.readlines()
+
         for column in columns:
             column = column.strip()
             row = [i for i in column]
@@ -45,24 +53,30 @@ class Maze:
         return maze
 
     def getPositionOf(self, char):
+
         for i in range(len(self.maze)):
             for j in range(len(self.maze[0])):
                 if self.maze[i][j] == char:
                     return i, j
 
     def setStartingPosition(self):
+
         return self.getPositionOf(self.starting_position_char)
 
     def setGoalPosition(self):
+
         return self.getPositionOf(self.goal_position_char)
 
     def getMaze(self):
+
         return self.maze
 
     def getCurrentPosition(self):
+
         return self.current_position_y, self.current_position_x
 
     def getGoalPosition(self):
+
         return self.goal_position
 
 
